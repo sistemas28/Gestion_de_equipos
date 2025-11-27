28f2d3ead4e14ffdc73443e919789c2712e97517
juanvalencia <juanvalencia19410@gmail.com>
2025-11-27 16:10:04 -0500

    hola

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
 
-app.listen(app.get('port'), '0.0.0.0', () => {
+app.listen(app.get('port'), () => {
     console.log("servidor escuchando en el puerto", app.get("port"));
 });
