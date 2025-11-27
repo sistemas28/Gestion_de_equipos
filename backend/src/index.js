2a40fe5863da59246b40ac436a3c49b1a25f5eaa
juanvalencia <juanvalencia19410@gmail.com>
2025-11-27 16:01:50 -0500

    desplegando backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), '0.0.0.0', () => {
     console.log("servidor escuchando en el puerto", app.get("port"));
 });
